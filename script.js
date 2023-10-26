89d80465f378aa388c6ae9c8936ab5c61bbd917a
Kyle James <140095432+kylejames1998@users.noreply.github.com>
2023-10-25 19:42:20 -0700

    added console logs

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log(8 * null); // 0
+console.log("5" + 1); // 51
+console.log("5" - 1); // 4
+console.log("five" * 2); // NaN
+console.log(false == 0); // true
+console.log(5 > 2 && 6 > 3); // true
+console.log(false == false ? "equal" : "not equal"); // equal
+console.log(true || false); // true
